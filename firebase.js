73f07cfdd13e0196df64ca212dd60b0aea350af7
TUSHAR JADHAV <145747304+Tushar0-02@users.noreply.github.com>
2025-04-07 12:13:48 +0530

    Update firebase.js
    for better use

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAqDVmtDc31ONWzORVTY6BZQBOOCFlrtNk",
-    authDomain: "presenty-7a139.firebaseapp.com",
-    projectId: "presenty-7a139",
-    storageBucket: "presenty-7a139.appspot.com",
-    messagingSenderId: "917953928211",
-    appId: "1:917953928211:web:52de43f23522ab881c73e6"
+  apiKey: "AIzaSyAqDVmtDc31ONWzORVTY6BZQBOOCFlrtNk",
+  authDomain: "presenty-7a139.firebaseapp.com",
+  databaseURL: "https://presenty-7a139-default-rtdb.firebaseio.com",
+  projectId: "presenty-7a139",
+  storageBucket: "presenty-7a139.appspot.com",
+  messagingSenderId: "917953928211",
+  appId: "1:917953928211:web:52de43f23522ab881c73e6"
 };
 
 const app = initializeApp(firebaseConfig);
